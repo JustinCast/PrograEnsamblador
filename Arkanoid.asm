4be1f2bd13b03d76f57c039e1cd66866a6ef193b
Justin Castillo <justincastillovalladares@gmail.com>
2016-10-27 19:39:35 -0600

    Nuevos cambios

--- Arkanoid.asm
+++ Arkanoid.asm
@@ -1,33 +1,8 @@
-.MODEL tiny ;tamano peque
-.DATA   
-    msg db 13,10,"Ingrese su nombre o apodo: $"    
-    nombre  DB  16 DUP('$')  
-.CODE
+stacksg segment para stack 'stack'      
+stacksg ends
+;---------------------------------
+datasg segment 'data'
+    ingresarApodo 10,13 "Ingrese su nombre o apodo $" 
+    apodo db ?                                       
+datasg ends
 
-Inicio:
-    ;Mover los datos al registro principal
-    mov ax, @data
-    mov dx, ax  
-    ;Mostrar 'Ingresar apodo'
-    mov dx, offset msg
-    mov ah, 9h
-    int 21h 
-    
-    ;Leer el nombre
-    mov ah,01h
-    int 21H
-    mov nombre, al
-    int 21h
-     
-    ;Mostrar el nombre
-    lea dx, nombre
-    mov ah, 9h
-    int 21h 
-    jmp Fin
-    
-    
-Fin:    
-    ;=============
-    ;Terminar Programa
-    mov ah,0x4C
-    int 0x21  
